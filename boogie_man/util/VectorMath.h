2267e3b536318300e9ab76a41f32da203a967cbd
KaganBaldiran <k.baldiran@hotmail.com>
2023-08-25 18:35:48 +0200

    Vector Class Update

--- boogie_man/util/VectorMath.h
+++ boogie_man/util/VectorMath.h
@@ -212,7 +212,7 @@ struct Vec3
 
 	friend std::ostream& operator<<(std::ostream& os, const Vec3& vec)
 	{
-		os << << "(" << vec.x << ", " << vec.y << ", "<< vec.z << ")";
+		os << "(" << vec.x << ", " << vec.y << ", "<< vec.z << ")";
 		return os;
 	}
 
