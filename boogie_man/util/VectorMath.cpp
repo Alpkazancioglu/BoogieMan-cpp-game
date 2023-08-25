5f62af8fb6402632aae5a3ab17791d342de76036
KaganBaldiran <k.baldiran@hotmail.com>
2023-08-25 16:44:00 +0200

    Vector class improvements

--- boogie_man/util/VectorMath.cpp
+++ boogie_man/util/VectorMath.cpp
@@ -1,5 +1,22 @@
 #include "VectorMath.h"
 
+//Infamous QUACK III inverse square root algorithm
+float Q_rsqrt(float number)
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y = number;
+	i = *(long*)&y;                       // evil floating point bit level hacking
+	i = 0x5f3759df - (i >> 1);               // what the fuck?
+	y = *(float*)&i;
+	y = y * (threehalfs - (x2 * y * y));   // 1st iteration
+		// y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+	return y;
+}
+
 int GiveRandomNumf(int min, int max, int sizeofarray, bool exclude_on_off, int numtoexclude) {
 
 	static int timesthisfunctioncalled = 0;
