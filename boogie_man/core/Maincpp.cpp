93041cf40f043bf0e4711852df4845019290f483
KaganBaldiran <k.baldiran@hotmail.com>
2023-08-27 14:51:52 +0200

    Include update

---
+++ boogie_man/core/Maincpp.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <raylib.h>
+#include <vector>
+#include <chrono>
+#include "GameHandler.h"
+#include "../util/FileHandler.h"
+
+game* currentgame;
+
+int main()
+{
+	currentgame = new game(512, 380);
+	currentgame->initialize("BoogieMan", 60);
+	while (!WindowShouldClose())
+	{
+
+		currentgame->update();
+
+		currentgame->draw();
+	}
+	
+	currentgame->Clean(currentgame);
+
+	return 0;
+}
